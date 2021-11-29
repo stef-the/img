eff11c64d6b14dfe2bef82fac36ca895d8219b5e
Stefan Leung <sleung25@FVFG7V5RQ6L4.local>
2021-11-29 15:12:01 -0800

    img38

--- test.py
+++ test.py
@@ -22,15 +22,6 @@ def scan(folder, jsonf):
     open(jsonf, 'w').write(json.dumps(a))
     return diff
 
-CMD = '''
-on run argv
-  display notification (item 2 of argv) with title (item 1 of argv)
-end run
-'''
-
-def notify(title, text):
-  subprocess.call(['osascript', '-e', CMD, title, text])
-
 while True:
     if scan('img', 'imglist.json'):
         a = str(int(open('iter.txt', 'r').read())+1)
